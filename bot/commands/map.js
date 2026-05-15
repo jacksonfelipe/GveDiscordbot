bc1aceaebab9148894d6448e74c1ccf6c24c82ff
jacksonfelipe <consultor.laurentino@gmail.com>
2026-05-14 22:20:45 -0300

    s

---
+++ bot/commands/map.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'map',
+    description: 'Envia o link do Mapa de Calor em tempo real',
+    async execute(message, args) {
+        const embed = new EmbedBuilder()
+            .setTitle('📍 Mapa de Calor em Tempo Real')
+            .setDescription('Veja onde o PVP está pegando fogo agora mesmo!')
+            .setThumbnail('https://cdn-icons-png.flaticon.com/512/854/854878.png')
+            .addFields({ 
+                name: '🔗 Link do Mapa', 
+                value: '[Clique aqui para abrir o Mapa](http://hub.l2jpremium.com.br/heatmap)' 
+            })
+            .setColor('#f39c12')
+            .setFooter({ text: 'Atualizado a cada 5 segundos' })
+            .setTimestamp();
+
+        message.reply({ embeds: [embed] });
+    },
+};
